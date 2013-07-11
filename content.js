5fb2b28e24ec0c70582ccc953ef03f7242e62657
Andre Zoufahl <zoufahl@zib.de>
2013-07-11 03:27:43 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,12 @@
+var CON = $("div#content");
+
+var sakaikvv = {
+	changeHeader: function() {
+		$("h2", CON).css({'font-size': '100%'});
+	}
+};
+
+
+console.log('content.js');
+sakaikvv.changeHeader();
+
